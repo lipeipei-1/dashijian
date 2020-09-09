35c9099780a785ffee3072c5678af7a161c4e7d4
lipeipei-1 <1416837697@qq.com>
2020-09-09 15:56:35 +0800

    完成了登录注册页面

---
+++ assets/js/login.js
@@ -0,0 +1,22 @@
+$(function() {
+    $('#link_reg').click(function() {
+        $('.login-box').hide()
+        $('.reg-box').show()
+
+    })
+    $('#link_login').click(function() {
+        $('.login-box').show()
+        $('.reg-box').hide()
+
+    })
+
+
+
+
+
+
+
+
+
+
+})
