1bf59fe6c15adaf3daaa95dc7dc03d149b4328e1
lipeipei-1 <1416837697@qq.com>
2020-09-15 16:14:53 +0800

    完成了发表功能

---
+++ assets/js/article/art_pub.js
@@ -0,0 +1,109 @@
+$(function() {
+    var layer = layui.layer
+    var form = layui.form
+        // 初始化富文本编辑器
+        // 1. 初始化图片裁剪器
+    var $image = $('#image')
+
+    // 2. 裁剪选项
+    var options = {
+        aspectRatio: 400 / 280,
+        preview: '.img-preview'
+    }
+
+    // 3. 初始化裁剪区域
+    $image.cropper(options)
+
+
+    initEditor()
+    initCate()
+
+    function initCate() {
+        $.ajax({
+            url: '/my/article/cates',
+            method: 'get',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                console.log(res);
+
+                var htmlstr = template('select-pub', res)
+                $('[name=cate_id]').html(htmlstr)
+                form.render()
+            }
+
+        })
+    }
+    //选择需要上传的图片
+    $('#selectImage').on("click", function() {
+        $('#file').click()
+    })
+
+
+    //替换照片
+    $('#file').on('change', function(e) {
+            //获取到图片信息
+            var files = e.target.files
+            if (files.length === 0) {
+                return layer.msg('上传图片失败')
+            }
+            //获取到上传的图片
+            var file = e.target.files[0]
+                //将上传的图片转换成URL格式
+            var newImgURL = URL.createObjectURL(file)
+            $image
+                .cropper('destroy') // 销毁旧的裁剪区域
+                .attr('src', newImgURL) // 重新设置图片路径
+                .cropper(options) // 重新初始化裁剪区域
+
+        })
+        //处理上传需要使用的数据
+    var art_state = '已发布'
+    $('#saveBtn2').on('click', function() {
+        var art_state = '草稿'
+    })
+
+
+    $('#form-pub').on('submit', function(e) {
+        e.preventDefault()
+        var fd = new FormData($(this)[0])
+        fd.append('state', art_state)
+        $image.cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function(blob) { // 将 Canvas 画布上的内容，转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                fd.append('cover_img', blob)
+                publishArticle(fd)
+            })
+            // fd.forEach(function(v, k) {
+            //     console.log(k, v);
+
+        // })
+
+    })
+
+    function publishArticle(fd) {
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/add',
+            data: fd,
+            // 注意：如果向服务器提交的是 FormData 格式的数据，
+            // 必须添加以下两个配置项
+            contentType: false,
+            processData: false,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('发布文章失败！')
+                }
+                layer.msg('发布文章成功！')
+                    // 发布文章成功后，跳转到文章列表页面
+                location.href = '../../../article/art_list.html'
+            }
+        })
+    }
+
+
+})
